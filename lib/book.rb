4878607997b49357a1e51f1054daff0c75007384
Ryan <ryanbiy01@gmail.com>
2023-05-20 21:05:58 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
-
+class book
+    attr_accessor :title, :author
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
